076b20cf4b60bb9377fd4714f90a950a806ddb50
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-18 06:42:10 -0700

    Implemented a State Machine using XState

--- src/App.js
+++ src/App.js
@@ -37,9 +37,19 @@ const App = () => {
 
     service.onTransition(state => {
       console.log(state);
-      // Your code here to change lightStatus when the 
-      //   state of the state machine changes
-
+      switch (state.value) {
+        case "green":
+          setLightStatus(GREEN_LIGHT);
+          break;
+        case "yellow":
+          setLightStatus(YELLOW_LIGHT);
+          break;
+        case "red":
+          setLightStatus(RED_LIGHT);
+          break;
+        default:
+          console.log("Error: invalid state");
+      }
     });
   }, [])
 
